e289192dae65acd79f04186eac230ccbf8ce8920
fabifors <fabian.forsstrom.com>
2019-05-15 19:54:34 +0200

    Added test console log

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.listen(PORT, function() {
 
 // This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
 app.get('/', function(req, res) {
+  console.log('This works man!')
   res.send(
     'Ngrok is working! Path Hit: '
     // '<a href="https://slack.com/oauth/authorize?scope=incoming-webhook&client_id=230513850368.604545361031"><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
